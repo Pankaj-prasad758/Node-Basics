9d75956151b98b2e56b17f6717ccb9a06d6e35e8
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2025-08-09 00:45:55 +0530

    Refactor fs import and add timer examples
    Renamed the imported fs module to fileSystem and added examples demonstrating setTimeout and setImmediate usage for logging asynchronous messages.

--- NodeWorking.js
+++ NodeWorking.js
@@ -1,5 +1,8 @@
-import fs from "fs"
+import fs from "fs";
 
-const fs = fs;
+const fileSystem = fs;
 
-
+console.log(`Top level elements`);
+setTimeout(() => console.log(`Non Top level code setTimeout`), 0);
+setTimeout(() => console.log(`Non Top level code setTimeout 2`), 0);
+setImmediate(() => console.log(`setImmediate timer`));
